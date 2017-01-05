5d915e2243ed1ff019f38df059396ec88921348b
dhknarola <dhk@narola.email>
2017-01-05 12:19:51 +0530

    DHK commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>If you would like tovbfdgdf ghdfgd edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
