f6ea0a920b6178c1955816742e072fca3d2dc8e5
dhknarola <dhk@narola.email>
2017-01-05 15:59:28 +0530

    new chages

--- application/controllers/admin/Blogs.php
+++ application/controllers/admin/Blogs.php
@@ -2,11 +2,11 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Users extends MY_Controller {
+class Blogs extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model(['admin/Admin_users_model', 'Users_model']);
+        $this->load->model(['admin/Admin_blogs_model']);
         $this->load->library(['encryption', 'upload']);
     }
 
@@ -14,12 +14,8 @@ class Users extends MY_Controller {
      * Function load view of users list.(HPA)
      */
     public function index() {
-        $session_data = $this->session->userdata('admin');
-        $data['user_data'] = $this->Users_model->check_if_user_exist(['id' => $session_data['id'], 'role_id' => 1], false, true);
-        if (empty($data['user_data'])) {
-            redirect('admin/login');
-        }        
-        $data['subview'] = 'admin/users/index';
+       
+        $data['subview'] = 'admin/blogs/index';
         $this->load->view('admin/layouts/layout_main', $data);
     }
 
@@ -27,16 +23,11 @@ class Users extends MY_Controller {
      * Function is used to get result based on datatable in user list page
      */
     public function list_user() {
-        $session_data = $this->session->userdata('admin');
-        $data['user_data'] = $this->Users_model->check_if_user_exist(['id' => $session_data['id'], 'role_id' => 1], false, true);
-        if (empty($data['user_data'])) {
-            redirect('admin/login');
-        }
-        $final['recordsTotal'] = $this->Admin_users_model->get_users_count();
+       
+        $final['recordsTotal'] = $this->Admin_blogs_model->get_blogs_count();
         $final['redraw'] = 1;
-        // $final['recordsFiltered'] = $this->admin_users_model->get_users_result(TBL_USER,$select,'count');
         $final['recordsFiltered'] = $final['recordsTotal'];
-        $final['data'] = $this->Admin_users_model->get_all_users();
+        $final['data'] = $this->Admin_blogs_model->get_all_blogs();
         echo json_encode($final);
         
 
