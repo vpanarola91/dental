d7dddfbff0db616e7742a9f6b37680b88c8716d9
dhknarola <dhk@narola.email>
2017-01-12 12:41:39 +0530

    Front Side Template

---
+++ application/views/front/home.php
@@ -0,0 +1,598 @@
+ <!-- REVOLUTION SLIDER -->
+    <link href="<?= DEFAULT_PLUGINS_PATH ?>slider.revolution/css/extralayers.css" rel="stylesheet" type="text/css" />
+    <link href="<?= DEFAULT_PLUGINS_PATH ?>slider.revolution/css/settings.css" rel="stylesheet" type="text/css" />
+
+
+<!-- REVOLUTION SLIDER -->
+<div class="slider fullwidthbanner-container roundedcorners">
+<!--
+	Navigation Styles:
+	
+		data-navigationStyle="" theme default navigation
+		
+		data-navigationStyle="preview1"
+		data-navigationStyle="preview2"
+		data-navigationStyle="preview3"
+		data-navigationStyle="preview4"
+		
+	Bottom Shadows
+		data-shadow="1"
+		data-shadow="2"
+		data-shadow="3"
+		
+	Slider Height (do not use on fullscreen mode)
+		data-height="300"
+		data-height="350"
+		data-height="400"
+		data-height="450"
+		data-height="500"
+		data-height="550"
+		data-height="600"
+		data-height="650"
+		data-height="700"
+		data-height="750"
+		data-height="800"
+	-->
+	<div class="fullwidthbanner" data-height="600" data-shadow="0" data-navigationStyle="preview2">
+		<ul class="hide">
+
+			<!-- SLIDE  -->
+			<li data-transition="parallaxtobottom" data-slotamount="7" data-masterspeed="600"  data-saveperformance="off"  data-title="Architectural View">
+				<!-- MAIN IMAGE -->
+				<img src="<?= DEFAULT_IMAGE_PATH ?>demo/lightblue-min.jpg"  alt="cover image"  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+
+				<div class="tp-caption white_big skewfromleft tp-resizeme"
+				data-x="500"
+				data-y="100" 
+				data-speed="400"
+				data-start="1500"
+				data-easing="Power3.easeInOut"
+				data-splitin="words"
+				data-splitout="none"
+				data-elementdelay="0.1"
+				data-endelementdelay="0.1"
+				data-endspeed="600"
+				style="z-index: 2; color:#fff; font-size:65px; line-height:85px; font-weight:bold; letter-spacing:0; text-shadow:none;">
+				We Care For Your
+			</div>
+
+			<div class="tp-caption content_text_center skewfromleft tp-resizeme"
+			data-x="500"
+			data-y="195" 
+			data-speed="500"
+			data-start="2000"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-endspeed="600"
+
+			style="z-index: 3; font-size:20px; color:#fff; font-weight:300; text-shadow:none;">
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit, <br />
+			sed diam nonummy nibh euismod tincidunt.
+		</div>
+
+		<div class="tp-caption tp-fade"
+		data-x="left"
+		data-y="25" 
+		data-speed="300"
+		data-start="800"
+		data-easing="Power3.easeInOut"
+		data-elementdelay="0.1"
+		data-endelementdelay="0.1"
+		data-endspeed="300"
+		style="z-index: 4;">
+		<img src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/doctor-min.png" alt="">
+	</div>
+
+	<div class="tp-caption skewfromleft tp-resizeme  un-button-2-lg"
+	data-x="500"
+	data-y="300" 
+	data-speed="300"
+	data-start="2500"
+	data-easing="Power3.easeInOut"
+	data-splitin="none"
+	data-splitout="none"
+	data-elementdelay="0.1"
+	data-endelementdelay="0.1"
+	data-endspeed="300"
+
+	style="z-index: 5; max-width: auto; max-height: auto; white-space: nowrap;">
+	<a class="btn btn-default btn-lg scrollTo" href="#appointment">MAKE AN APPOINTMENT &nbsp; <i class="fa fa-angle-right"></i></a>
+</div>
+</li>
+
+<!-- SLIDE  -->
+<li data-transition="parallaxtotop" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" data-title="Bright Future">
+	<!-- MAIN IMAGE -->
+	<img src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/slider_2-min.jpg" alt="cover image"  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+
+	<div class="overlay dark-1"><!-- dark overlay [0 to 9 opacity] --></div>
+
+	<div class="tp-caption font-roboto skewfromleft tp-resizeme"
+	data-x="50"
+	data-y="100" 
+	data-speed="500"
+	data-start="1500"
+	data-easing="Cubic.easeOut"
+	data-splitin="none"
+	data-splitout="none"
+	data-elementdelay="0.1"
+	data-endelementdelay="0.1"
+	data-endspeed="500"
+	style="z-index: 2; color:#fff; font-size:65px; line-height:85px; font-weight:bold; letter-spacing:0; text-shadow:none;">
+	Creating a Bright Future <br />
+	Together
+</div>
+
+<div class="tp-caption skewfromrightshort tp-resizeme"
+data-x="50"
+data-y="300" 
+data-speed="500"
+data-start="1000"
+data-easing="easeInCirc"
+data-splitin="none"
+data-splitout="none"
+data-elementdelay="0.1"
+data-endelementdelay="0.1"
+data-endspeed="500"
+style="z-index: 3; font-size:20px; color:#fff; font-weight:300; text-shadow:none;">
+If you dream of designing a new home that takes full advantage, <br />
+our team is the best in this field.
+</div>
+
+<div class="tp-caption sfb tp-resizeme"
+data-x="50"
+data-y="410" 
+data-speed="500"
+data-start="1500"
+data-easing="Power3.easeIn"
+data-splitin="none"
+data-splitout="none"
+data-elementdelay="1"
+data-endelementdelay="0.1"
+data-endspeed="500"
+style="z-index: 4; max-width: auto;">
+<a class="btn btn-primary btn-lg" href="#">OUR SERVICES &nbsp; <i class="fa fa-angle-right"></i></a>
+</div>
+
+<div class="tp-caption sfb tp-resizeme"
+data-x="250"
+data-y="410" 
+data-speed="500"
+data-start="1500"
+data-easing="Power3.easeInOut"
+data-splitin="none"
+data-splitout="none"
+data-elementdelay="0.1"
+data-endelementdelay="0.1"
+data-endspeed="500"
+style="z-index: 6; max-width: auto; max-height: auto; white-space: nowrap;">
+<a class="btn btn-default btn-lg scrollTo" href="#appointment">MAKE AN APPOINTMENT &nbsp; <i class="fa fa-angle-right"></i></a>
+</div>
+</li>
+
+</ul>
+
+</div>
+</div>
+<!-- /REVOLUTION SLIDER -->
+
+
+<hr class="nomargin" /><!-- 1px line separator -->
+
+
+<!-- BUTTON CALLOUT -->
+<a href="#" class="btn btn-xlg btn-info size-20 fullwidth nomargin noradius padding-40">
+	<span class="font-lato size-30">
+		Do you have questions? 
+		<strong>Contact us &raquo;</strong>
+	</span>
+</a>
+<!-- /BUTTON CALLOUT -->
+
+
+
+<!-- Overview -->
+<section>
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-lg-4 col-md-4 col-sm-12 ">
+				<h2>Health and Medical</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+
+				<a class="btn btn-danger btn-lg" href="#">VIEW OUR SERVICES</a>
+			</div>
+
+			<div class="col-lg-8 col-md-8 col-sm-12">
+				<h3 class="weight-300">Quick <span>Overview</span></h3>
+
+				<p>Lorem ipsum dolor sit amet, hinc dolores noluisse at vel, ex quod aperiri scaevola has, cum te sanctus denique. Eu liber docendi petentium has, labore.</p>
+				<p>Etiam falli mentitum id mel, ut mel sumo postulant referrentur. At has minim imperdiet, soluta offendit ocurreret sea an. </p>
+
+				<hr />
+
+				<ul class="list-unstyled list-icons">
+					<li><i class="fa fa-check"></i> Nobis nemore epicuri pro ea</li>
+					<li><i class="fa fa-check"></i> Qui dicunt singulis dissentias an</li>
+					<li><i class="fa fa-check"></i> Ea vis diceret constituam</li>
+					<li><i class="fa fa-check"></i> Mei no autem idque integre, sumo facilis</li>
+					<li><i class="fa fa-check"></i> Est quodsi labitur moderatius an</li>
+				</ul>
+
+			</div>
+		</div>
+
+	</div>
+</section>
+<!-- /Overview -->
+
+
+
+
+<!-- Parallax -->
+<section class="parallax parallax-1" style="background-image: url('<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/slider_2-min.jpg');">
+	<div class="overlay dark-5"><!-- dark overlay [1 to 9 opacity] --></div>
+
+	<div class="container">
+
+		<div class="text-center">
+			<h2 class="size-40 weight-300">Medical Services That You Can Trust</h2>
+			<a class="btn btn-danger btn-lg" href="#">MAKE AN APPOINTMENT</a>
+		</div>
+
+	</div>
+</section>
+<!-- /Parallax -->
+
+
+
+
+<!-- Team -->
+<section>
+	<div class="container">
+
+		<header class="text-center margin-bottom-60 clearfix">
+			<h2 class="weight-300 nomargin-bottom">Meet Our Specialists</h2>
+		</header>
+
+		<div class="row">
+
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/t1-min.jpg" alt="" />
+					<div class="caption">
+						<h4 class="nomargin">John Doe</h4>
+						<small class="margin-bottom-20 block">Web Developer</small>
+
+						<p>Donec id elit non mi porta gravida at eget metus id dolor.</p>
+						<a href="#" class="btn btn-default btn-sm">READ MORE</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/t2-min.jpg" alt="" />
+					<div class="caption">
+						<h4 class="nomargin">Angelina Doe</h4>
+						<small class="margin-bottom-20 block">Architect</small>
+
+						<p>Donec id elit non mi porta gravida at eget metus id dolor.</p>
+						<a href="#" class="btn btn-default btn-sm">READ MORE</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/t3-min.jpg" alt="" />
+					<div class="caption">
+						<h4 class="nomargin">Mark Doe</h4>
+						<small class="margin-bottom-20 block">Web Designer</small>
+
+						<p>Donec id elit non mi porta gravida at eget metus id dolor.</p>
+						<a href="#" class="btn btn-default btn-sm">READ MORE</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-3">
+				<div class="thumbnail">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/thematics/medical/t4-min.jpg" alt="" />
+					<div class="caption">
+						<h4 class="nomargin">Jolie Doe</h4>
+						<small class="margin-bottom-20 block">Art Director</small>
+
+						<p>Donec id elit non mi porta gravida at eget metus id dolor.</p>
+						<a href="#" class="btn btn-default btn-sm">READ MORE</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+
+	</div>
+</section>
+<!-- /Team -->
+
+
+
+
+<!-- Testimonials -->
+<section class="padding-xxs dark">
+	<div class="container">
+
+		<div class="owl-carousel text-center owl-testimonial nomargin" data-plugin-options='{"singleItem": true, "autoPlay": 3500, "navigation": false, "pagination": true, "transitionStyle":"fade"}'>
+			<div class="testimonial">
+				<figure>
+					<img class="rounded" src="<?= DEFAULT_IMAGE_PATH ?>demo/people/300x300/11-min.jpg" alt="" />
+				</figure>
+				<div class="testimonial-content nopadding">
+					<p class="lead">Incidunt deleniti blanditiis quas aperiam recusandae consequatur ullam quibusdam cum libero.</p>
+					<cite>
+						Joana Doe
+						<span>Company Ltd.</span>
+					</cite>
+				</div>
+			</div>
+			<div class="testimonial">
+				<figure>
+					<img class="rounded" src="<?= DEFAULT_IMAGE_PATH ?>demo/people/300x300/12-min.jpg" alt="" />
+				</figure>
+				<div class="testimonial-content nopadding">
+					<p class="lead">Quod necessitatibus quis expedita harum provident eos obcaecati id culpa.</p>
+					<cite>
+						Melissa Doe
+						<span>Company Ltd.</span>
+					</cite>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+<!-- /Testimonials -->
+
+
+
+
+
+<!-- News -->
+<section>
+	<div class="container">
+
+		<header class="text-center margin-bottom-60 clearfix">
+			<h2 class="weight-300 nomargin-bottom">Recent News</h2>
+		</header>
+
+		<!-- 
+			controlls-over		= navigation buttons over the image 
+			buttons-autohide 	= navigation buttons visible on mouse hover only
+			
+			data-plugin-options:
+				"singleItem": true
+				"autoPlay": true (or ms. eg: 4000)
+				"navigation": true
+				"pagination": true
+				"items": "4"
+
+			owl-carousel item paddings
+				.owl-padding-0
+				.owl-padding-3
+				.owl-padding-6
+				.owl-padding-10
+				.owl-padding-15
+				.owl-padding-20
+		-->
+		<div class="owl-carousel owl-padding-10 buttons-autohide controlls-over" data-plugin-options='{"singleItem": false, "items":"4", "autoPlay": 4000, "navigation": true, "pagination": false}'>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/24-min.jpg" alt="">
+				</a>
+
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/17-min.jpg" alt="">
+				</a>
+
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/30-min.jpg" alt="">
+				</a>
+
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/26-min.jpg" alt="">
+				</a>
+
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/18-min.jpg" alt="">
+				</a>
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/34-min.jpg" alt="">
+				</a>
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/37-min.jpg" alt="">
+				</a>
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="img-hover">
+				<a href="blog-single-default.html">
+					<img class="img-responsive" src="<?= DEFAULT_IMAGE_PATH ?>demo/451x300/23-min.jpg" alt="">
+				</a>
+				<h4 class="text-left margin-top-20"><a href="blog-single-default.html">Lorem Ipsum Dolor</a></h4>
+				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in.</p>
+				<ul class="text-left size-12 list-inline list-separator">
+					<li>
+						<i class="fa fa-calendar"></i> 
+						29th Jan 2015
+					</li>
+					<li>
+						<a href="blog-single-default.html#comments">
+							<i class="fa fa-comments"></i> 
+							3
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+</section>
+<!-- /News -->
+
+
+
+
+
+<!-- APPOINTMENT -->
+<section id="appointment" class="alternate">
+	<div class="container">
+
+		<header class="text-center margin-bottom-60 clearfix">
+			<h2 class="weight-300 nomargin-bottom">Make An Appointment</h2>
+		</header>
+
+		<form class="validate" action="php/contact.php" method="post" data-success="Reservation sent! Thank you!" data-toastr-position="bottom-right">
+			<div class="row">
+				<div class="col-md-4">
+					<input type="text" id="appointment_name" name="appointment_name" class="form-control required" placeholder="Name*">
+				</div>
+
+				<div class="col-md-4">
+					<input type="email" id="appointment_email" name="appointment_email" class="form-control required" placeholder="Email*">
+				</div>
+
+				<div class="col-md-4">
+					<input type="text" id="appointment_phone" name="appointment_phone" class="form-control required" placeholder="Phone*">
+				</div>
+
+			</div>
+			<div class="row">
+
+				<div class="col-md-4">
+					<input type="text" id="appointment_date" name="appointment_date" class="form-control datepicker required" data-format="yyyy-mm-dd" data-lang="en" data-RTL="false" placeholder="Appointment Date*">
+				</div>
+
+				<div class="col-md-8">
+					<input type="text" id="appointment_message" name="appointment_message" class="form-control required" placeholder="Appointment Message*">
+				</div>
+
+			</div>
+
+			<button class="btn btn-success" type="submit">SUBMIT APPOINTMENT</button>
+
+		</form>
+
+	</div>
+</section>
+<!-- /APPOINTMENT -->
---
+++ application/views/front/layouts/layout_header.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Dental</title>
+    <meta name="keywords" content="Dental" />
+    <meta name="description" content="Dental Website" />
+    <meta name="Author" content="Dental" />
+
+    <!-- mobile settings -->
+    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0" />
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+    <!-- WEB FONTS : use %7C instead of | (pipe) -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400%7CRaleway:300,400,500,600,700%7CLato:300,400,400italic,600,700" rel="stylesheet" type="text/css" />
+
+    <!-- CORE CSS -->
+    <link href="<?= DEFAULT_PLUGINS_PATH ?>bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+   
+    <!-- THEME CSS -->
+    <link href="<?= DEFAULT_CSS_PATH ?>essentials.css" rel="stylesheet" type="text/css" />
+    <link href="<?= DEFAULT_CSS_PATH ?>layout.css" rel="stylesheet" type="text/css" />
+
+    <!-- PAGE LEVEL SCRIPTS -->
+    <link href="<?= DEFAULT_CSS_PATH ?>header-1.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="<?= DEFAULT_CSS_PATH ?>color_scheme/blue.css" rel="stylesheet" type="text/css" id="color_scheme" /> -->
+</head>
